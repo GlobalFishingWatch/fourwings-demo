b6a41d7345b9c46b0954707cd88f274b88467443
nerik <erik.escoffier@gmail.com>
2020-02-11 13:05:52 +0100

    build

---
+++ precache-manifest.c1b8e80344cf98e0bb1a0fe0329d2b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c217acef3aa551b3079c6c9c6bae3b86",
+    "url": "/fourwings-demo/index.html"
+  },
+  {
+    "revision": "48f989657ae93603a06c",
+    "url": "/fourwings-demo/static/css/main.65fa45d0.chunk.css"
+  },
+  {
+    "revision": "f5fbcc4648ea39c87877",
+    "url": "/fourwings-demo/static/js/2.06dd7e05.chunk.js"
+  },
+  {
+    "revision": "48f989657ae93603a06c",
+    "url": "/fourwings-demo/static/js/main.398bd74c.chunk.js"
+  },
+  {
+    "revision": "c2e2838ce98f0e9ddb61",
+    "url": "/fourwings-demo/static/js/runtime~main.8e8390dd.js"
+  }
+]);
