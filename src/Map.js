28119caa3b55b6564637de5b81403ae5c69709da
satellitestudiodesign <juancarlos@satellitestud.io>
2020-02-11 10:20:25 +0100

    cosas

--- src/Map.js
+++ src/Map.js
@@ -1,207 +1,270 @@
-import React, {useState, useMemo, useEffect, useRef} from 'react';
-import ReactMapGL, { Popup } from 'react-map-gl';
-import useMapStyler from '@globalfishingwatch/map-components/components/layer-composer-hook'
-import LayerComposer, { sort, defaultGenerators, TYPES, HEATMAP_GEOM_TYPES, HEATMAP_COLOR_RAMPS } from '@globalfishingwatch/layer-composer'
-import Timebar from '@globalfishingwatch/map-components/components/timebar'
-import styles from './Map.css'
-import TrackGenerator from './TrackGenerator'
+import React, { useState, useMemo, useEffect, useRef } from "react";
+import ReactMapGL, { Popup } from "react-map-gl";
+import useMapStyler from "@globalfishingwatch/map-components/components/layer-composer-hook";
+import LayerComposer, {
+  sort,
+  defaultGenerators,
+  TYPES,
+  HEATMAP_GEOM_TYPES,
+  HEATMAP_COLOR_RAMPS
+} from "@globalfishingwatch/layer-composer";
+import Timebar from "@globalfishingwatch/map-components/components/timebar";
+import styles from "./Map.css";
+import TrackGenerator from "./TrackGenerator";
 
 const layerComposer = new LayerComposer({
   generators: {
     ...defaultGenerators,
-    track: new TrackGenerator(),
-  },
-})
-const styleTransformations = [sort]
+    track: new TrackGenerator()
+  }
+});
+const styleTransformations = [sort];
 
 const Map = () => {
   const [viewport, setViewport] = useState({
     latitude: 27,
     longitude: -77,
     zoom: 4
-  })
-  const [geomType, setGeomType] = useState('grid')
-  const [fishingVisible, setFishingVisible] = useState(true)
-  const [tracksVisible, setTracksVisible] = useState(false)
-  const [usingTimebar, setUsingTimebar] = useState(false)
-  const [dates, setDates] = useState({ start: '2019-04-01T00:00:00.000Z', end: '2019-08-01T00:00:00.000Z' });
-  
-  const [idle, setIdle] = useState(false)
-  const mapRef = useRef()
+  });
+  const [geomType, setGeomType] = useState("grid");
+  const [fishingVisible, setFishingVisible] = useState(true);
+  const [tracksVisible, setTracksVisible] = useState(false);
+  const [temperatureVisible, setTemperatureVisible] = useState(false);
+  const [usingTimebar, setUsingTimebar] = useState(false);
+  const [dates, setDates] = useState({
+    start: "2019-09-01T00:00:00.000Z",
+    end: "2019-10-01T00:00:00.000Z"
+  });
+
+  const [idle, setIdle] = useState(false);
+  const mapRef = useRef();
   useEffect(() => {
-    const map = mapRef.current.getMap()
-    map.on('idle', () => {
-      console.log('idle')
-      setIdle(true)
-    })
+    const map = mapRef.current.getMap();
+    map.on("idle", () => {
+      console.log("idle");
+      setIdle(true);
+    });
     return () => {
-      map.off('idle')
-    }
-  }, [mapRef])
-  
+      map.off("idle");
+    };
+  }, [mapRef]);
+
   const styleConfig = useMemo(() => {
     let config = [
       {
-        id: 'background',
+        id: "background",
         type: TYPES.BACKGROUND,
-        color: '#0c276c'
+        color: temperatureVisible ? "#000" : "#0c276c"
       },
-      // {
-      //   type: TYPES.BASEMAP,
-      //   id: 'graticules',
-      // },
       {
         type: TYPES.BASEMAP,
-        id: 'landmass',
+        id: "graticules"
       }
-    ]
+      // {
+      //   type: TYPES.BASEMAP,
+      //   id: "landmass"
+      // }
+    ];
 
     if (fishingVisible) {
-      config = [...config,
+      config = [
+        ...config,
         {
-          id: 'heatmap-anim',
+          id: "heatmap-anim",
           type: TYPES.HEATMAP_TYPE,
-          tileset: 'fishing_demo',
+          tileset: "fishing_demo",
           start: dates.start,
           end: dates.end,
           zoom: viewport.zoom,
-          opacity: (usingTimebar || !idle) ? 1 : .001,
-          visible: true,
-          colorRampMult: .4,
+          // opacity: usingTimebar || !idle ? 1 : 0.001,
+          // visible: true,
+          visible: usingTimebar,
+          colorRampMult: 0.4,
           colorRamp: HEATMAP_COLOR_RAMPS.FISHING,
           geomType: HEATMAP_GEOM_TYPES.BLOB,
           updateColorRampOnTimeChange: false,
-          singleFrame: false,
+          singleFrame: false
         },
         {
-          id: 'heatmap',
+          id: "heatmap",
           type: TYPES.HEATMAP_TYPE,
-          tileset: 'fishing_demo',
+          tileset: "fishing_demo",
           start: dates.start,
           end: dates.end,
           zoom: viewport.zoom,
           visible: !usingTimebar,
-          colorRampMult: .4,
+          colorRampMult: 0.4,
           colorRamp: HEATMAP_COLOR_RAMPS.FISHING,
-          geomType: (geomType === 'extruded') ? HEATMAP_GEOM_TYPES.EXTRUDED : HEATMAP_GEOM_TYPES.GRIDDED,
+          geomType:
+            geomType === "extruded"
+              ? HEATMAP_GEOM_TYPES.EXTRUDED
+              : HEATMAP_GEOM_TYPES.GRIDDED,
           updateColorRampOnTimeChange: false,
-          singleFrame: false,
-        },
-      ]
+          singleFrame: false
+        }
+      ];
     }
-    
+
     if (tracksVisible) {
-      config = [...config, 
+      config = [
+        ...config,
         {
-          id: 'track',
-          type: 'track',
+          id: "track",
+          type: "track",
           start: dates.start,
           end: dates.end,
           index: 1,
-          color: '#ff00ff'
+          color: "#ff00ff"
         },
         {
-          id: 'track2',
-          type: 'track',
+          id: "track2",
+          type: "track",
           start: dates.start,
           end: dates.end,
           index: 2,
-          color: '#ff0000'
+          color: "#ff0000"
         },
         {
-          id: 'track3',
-          type: 'track',
+          id: "track3",
+          type: "track",
           start: dates.start,
           end: dates.end,
           index: 3,
-          color: '#ffff00'
-        },
-      ]
+          color: "#ffff00"
+        }
+      ];
     }
-    return config
+    return config;
+  }, [
+    dates.end,
+    dates.start,
+    fishingVisible,
+    geomType,
+    temperatureVisible,
+    tracksVisible,
+    usingTimebar,
+    viewport.zoom
+  ]);
 
-  }, [dates.end, dates.start, fishingVisible, geomType, idle, tracksVisible, usingTimebar, viewport.zoom])
+  const [style] = useMapStyler(
+    layerComposer,
+    styleTransformations,
+    styleConfig
+  );
+  const [highlighted, setHighlighted] = useState(null);
 
-  const [style] = useMapStyler(layerComposer, styleTransformations, styleConfig)
-  const [highlighted, setHighlighted] = useState(null)
+  const currentlyAt =
+    style &&
+    fishingVisible &&
+    style.layers[2] &&
+    style.layers[2].metadata &&
+    style.layers[2].metadata.currentlyAt;
 
-  const currentlyAt = style && fishingVisible && style.layers[2] && style.layers[2].metadata && style.layers[2].metadata.currentlyAt
-
-
-
-
-  return <div className="container">
-    <ul className="layers">
-      <li>
-        <input type="checkbox" checked={tracksVisible} onChange={(event) => setTracksVisible(event.target.checked)} />Shark tracks
-      </li>
-      <li>
-        <input type="checkbox" checked={fishingVisible} onChange={(event) => setFishingVisible(event.target.checked)}/>Fishing activity
-          <select onChange={(event) => setGeomType(event.target.value)}>
+  return (
+    <div className="container">
+      <ul className="layers">
+        <img
+          src="https://globalfishingwatch.org/wp-content/uploads/logolight_small-1.png"
+          srcset="https://globalfishingwatch.org/wp-content/uploads/logolight_small-1.png 1x, https://globalfishingwatch.org/wp-content/uploads/logolight_retina-1.png 2x"
+          width="210"
+          height="55"
+          alt="Global Fishing Watch Logo"
+          retina_logo_url="https://globalfishingwatch.org/wp-content/uploads/logolight_retina-1.png"
+        ></img>
+        <li>
+          <input
+            type="checkbox"
+            checked={tracksVisible}
+            onChange={event => setTracksVisible(event.target.checked)}
+          />
+          Sharks Telemetry Tracks
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            checked={fishingVisible}
+            onChange={event => setFishingVisible(event.target.checked)}
+          />
+          AIS Fishing Activity
+          {/* <select onChange={event => setGeomType(event.target.value)}>
             <option value="grid">grid</option>
             <option value="extruded">extruded</option>
-          </select>
-      </li>
-      <li>
-        <input type="checkbox" />Surface temperature 
-      </li>
-    </ul>
-    <div className="map">
-      <ReactMapGL
-        ref={mapRef}
-        width='100%'
-        height='100%'
-        {...viewport}
-        onViewportChange={(viewport) => setViewport(viewport)}
-        mapStyle={style}
-        interactiveLayerIds={fishingVisible ? ['heatmap'] : []}
-        onHover={(e) => {
-          if (e.features && e.features.length && currentlyAt) {
-            const info = e.features[0].properties.info.split(',')
-            const valueAt = e.features[0].properties[currentlyAt]
-            if (valueAt) {
-              setHighlighted({
-                // value: e.features[0].properties[currentlyAt],
-                value: info[currentlyAt],
-                lngLat: e.lngLat
-              })
-              return
+          </select> */}
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            checked={temperatureVisible}
+            onChange={event => setTemperatureVisible(event.target.checked)}
+          />
+          Sea Surface Temperature
+        </li>
+      </ul>
+      <div className="map">
+        <ReactMapGL
+          ref={mapRef}
+          width="100%"
+          height="100%"
+          {...viewport}
+          onViewportChange={viewport => setViewport(viewport)}
+          mapStyle={style}
+          interactiveLayerIds={fishingVisible ? ["heatmap"] : []}
+          onHover={e => {
+            if (e.features && e.features.length && currentlyAt) {
+              const info = e.features[0].properties.info.split(",");
+              const valueAt = e.features[0].properties[currentlyAt];
+              if (valueAt) {
+                setHighlighted({
+                  // value: e.features[0].properties[currentlyAt],
+                  value: info[currentlyAt],
+                  lngLat: e.lngLat
+                });
+                return;
+              }
             }
-          }
-          setHighlighted(null)
+            setHighlighted(null);
+          }}
+          mapOptions={{ hash: true, showTileBoundaries: true }}
+        >
+          {highlighted && (
+            <Popup
+              longitude={highlighted.lngLat[0]}
+              latitude={highlighted.lngLat[1]}
+              closeButton={false}
+              closeOnClick={false}
+              anchor="top"
+            >
+              <div className="popup">{highlighted.value} vessel positions</div>
+            </Popup>
+          )}
+        </ReactMapGL>
+      </div>
+      <div
+        className="timebar"
+        onMouseEnter={() => {
+          setUsingTimebar(true);
         }}
-        mapOptions={{ hash: true, showTileBoundaries: true }}
-      >
-        {highlighted && <Popup
-          longitude={highlighted.lngLat[0]}
-          latitude={highlighted.lngLat[1]}
-          closeButton={false}
-          closeOnClick={false}
-          anchor="top">
-            <div className="popup">
-              {highlighted.value} vessel positions
-            </div>
-        </Popup>}
-      </ReactMapGL>
-    </div>
-    <div  className="timebar" onMouseEnter={() => {setUsingTimebar(true);}}
-        onMouseLeave={() => { setUsingTimebar(false); setIdle(false)}}>
-      <Timebar
-        enablePlayback
-        start={dates.start}
-        end={dates.end}
-        absoluteStart={'2019-01-01T00:00:00.000Z'}
-        absoluteEnd={'2020-01-01T00:00:00.000Z'}
-        onChange={(start, end) => { 
-          setDates({
-            start,
-            end
-          })
+        onMouseLeave={() => {
+          setUsingTimebar(false);
+          setIdle(false);
         }}
       >
-      </Timebar>
+        <Timebar
+          enablePlayback
+          start={dates.start}
+          end={dates.end}
+          absoluteStart={"2019-01-01T00:00:00.000Z"}
+          absoluteEnd={"2020-01-01T00:00:00.000Z"}
+          onChange={(start, end) => {
+            setDates({
+              start,
+              end
+            });
+          }}
+        ></Timebar>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default Map
+export default Map;
--- src/TrackGenerator.js
+++ src/TrackGenerator.js
@@ -1,61 +1,61 @@
-import data1 from './track'
-import data2 from './track2'
-import data3 from './track3'
+import data1 from "./track";
+import data2 from "./track2";
+import data3 from "./track3";
 
 class TrackGenerator {
-  type = 'track'
+  type = "track";
 
-  _getStyleSources = (config) => {
-    const startTs = new Date(config.start).getTime()
-    const endTs = new Date(config.end).getTime()
-    const points = [null,data1,data2,data3][config.index].features
+  _getStyleSources = config => {
+    const startTs = new Date(config.start).getTime();
+    const endTs = new Date(config.end).getTime();
+    const points = [null, data1, data2, data3][config.index].features;
     const track = {
-      "type": "FeatureCollection",
-      "features": [
+      type: "FeatureCollection",
+      features: [
         {
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "LineString",
-            "coordinates": []
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: []
           }
         }
       ]
-    }
+    };
 
     points.forEach(p => {
-      const ts = new Date(p.properties.timestamp).getTime()
+      const ts = new Date(p.properties.timestamp).getTime();
       if (ts > startTs && ts < endTs) {
-        track.features[0].geometry.coordinates.push(p.geometry.coordinates)
+        track.features[0].geometry.coordinates.push(p.geometry.coordinates);
       }
-    })
+    });
 
     const source = {
-      type: 'geojson',
+      type: "geojson",
       data: track
-    }
-    return [{ id: config.id, ...source }]
-  }
-  _getStyleLayers = (config) => {
+    };
+    return [{ id: config.id, ...source }];
+  };
+  _getStyleLayers = config => {
     const layer = {
       id: `${config.id}_lines`,
       source: config.id,
-      type: 'line',
+      type: "line",
       paint: {
-        'line-width': 1,
-        'line-color': config.color,
-      },
-    }
-    return [layer]
-  }
+        "line-width": 2,
+        "line-color": config.color
+      }
+    };
+    return [layer];
+  };
 
-  getStyle = (config) => {
+  getStyle = config => {
     return {
       id: config.id,
       sources: this._getStyleSources(config),
-      layers: this._getStyleLayers(config),
-    }
-  }
+      layers: this._getStyleLayers(config)
+    };
+  };
 }
 
-export default TrackGenerator
+export default TrackGenerator;
